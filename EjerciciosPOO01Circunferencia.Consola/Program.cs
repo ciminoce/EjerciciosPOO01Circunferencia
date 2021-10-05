1459e71ab7e59b5f79d1d6c585231d54e33cf562
ciminoce <ciminoce@yahoo.com.ar>
2021-10-05 20:02:52 -0300

    Agregar archivos de proyecto.

---
+++ EjerciciosPOO01Circunferencia.Consola/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EjerciciosPOO01Circunferencia.Entidades;
+
+namespace EjerciciosPOO01Circunferencia.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Circunferencia c = new Circunferencia();//creo un obj de tipo Circunferencia
+                c.SetRadio(10);
+                Console.WriteLine($"Circ de radio {c.GetRadio()}");
+                Console.WriteLine($"Su perímetro es {c.GetPerimetro()}");
+                Console.WriteLine($"Su superficie es {c.GetSuperficie()}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            } 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EjerciciosPOO01Circunferencia.Entidades/Circunferencia.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosPOO01Circunferencia.Entidades
+{
+    public class Circunferencia
+    {
+        //Atributos
+        private int Radio;
+
+        //Propiedades
+        public void SetRadio(int value)
+        {
+            if (value > 0)
+            {
+                Radio = value;
+            }
+            else
+            {
+                throw new Exception("Valor del radio incorrecto!!!!");
+            }
+        }
+
+        public int GetRadio()
+        {
+            return Radio;
+        }
+        //Constructor
+        public Circunferencia()
+        {
+            
+        }
+
+        //Métodos
+
+        public double GetPerimetro()
+        {
+            return 2 * Math.PI * Radio;
+        }
+
+        public double GetSuperficie()
+        {
+            return Math.PI * Math.Pow(Radio, 2);
+        }
+    }
+}
